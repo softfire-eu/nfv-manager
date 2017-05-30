64bebdddc0fa52abe54e60f32bf025be9bf55cde
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-05-30 12:41:37 +0200

    new version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="nfv-manager",
-    version="0.1.3",
+    version="0.1.4",
     author="SoftFIRE",
     author_email="softfire@softfire.eu",
     description="The SoftFIRE NFV Manager",
