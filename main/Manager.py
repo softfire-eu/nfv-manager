b4a91badf64cbb261476ce5a3b877356f3fed447
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-05-31 20:07:44 +0200

    add checker thread

--- main/Manager.py
+++ main/Manager.py
@@ -1,27 +1,19 @@
-import socket
-
 from sdk.softfire.main import start_manager
 
-from core.NfvManager import NfvManager
-from utils.utils import get_logger
-
-
-def is_ex_man__running(ex_man_bind_ip, ex_man_bind_port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(2)
-    result = sock.connect_ex((ex_man_bind_ip, int(ex_man_bind_port)))
-    return result == 0
+from core.NfvManager import NfvManager, UpdateStatusThread
 
 
 def start():
-    # ex_man_bind_ip = config.get('system', 'experiment_manager_ip')
-    # ex_man_bind_port = config.get('system', 'experiment_manager_port')
-    # while not is_ex_man__running(ex_man_bind_ip, ex_man_bind_port):
-    #     time.sleep(1)
-    logger = get_logger(__name__)
-    # logger.info("Starting NFV Manager.")
-
-    start_manager(NfvManager(), '/etc/softfire/nfv-manager.ini')
+    nfv_manager = NfvManager('/etc/softfire/nfv-manager.ini')
+    thread = UpdateStatusThread(nfv_manager)
+    thread.start()
+    try:
+        start_manager(nfv_manager)
+        thread.join()
+    except:
+        thread.stop()
+        thread.join()
+        exit(0)
 
 
 if __name__ == '__main__':
