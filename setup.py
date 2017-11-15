a14cb8c18f7dec4f2cba84f537a3e70d525bfa15
ThomasBri <thomas.briedigkeit@fokus.fraunhofer.de>
2017-11-15 23:44:50 +0100

    Update to version 1.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="nfv-manager",
-    version="1.1.7",
+    version="1.2.0",
     author="SoftFIRE",
     author_email="softfire@softfire.eu",
     description="The SoftFIRE NFV Manager",
